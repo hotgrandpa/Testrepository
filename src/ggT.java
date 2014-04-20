60778459566e769dab6d22dd855634354d7f5ebc
Tristan Hahn <tristan.hahn@uni-duesseldorf.de>
2014-04-20 21:52:41 +0200

    modified ggt

--- src/ggT.java
+++ src/ggT.java
@@ -3,13 +3,12 @@
  */
 public class ggT {
     private static int ggt(int zahl1, int zahl2) {
-        while (zahl2 != 0) {
-            if (zahl1 > zahl2) {
-                zahl1 = zahl1 - zahl2;
-            } else {
-                zahl2 = zahl2 - zahl1;
-            }
+       if (zahl1 == zahl2){
+           return zahl1;
+       }
+        if (zahl1>zahl2){
+            return ggt(zahl2,(zahl1-zahl2));
         }
-        return zahl1;
+        return ggt(zahl2,(zahl2-zahl1));
     }
 }
